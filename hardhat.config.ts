55d9f2ed5abd2b40aadd5f376dfd496ad83e4dd9
Alejandro <smarketing.cuz@gmail.com>
2024-09-03 01:14:18 -0500

    new dependencies added

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    "linea-testnet": {
+      url: `https://linea-sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY!],
+      chainId: 59141,
+    },
+  },
 };
 
 export default config;
